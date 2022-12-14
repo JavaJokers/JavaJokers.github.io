07365ac4e44a59cb2b531bd3029b5cacebb180a9
20KrostagAlex07 <krostagalex@gmail.com>
2022-12-14 20:45:53 +0000

    Delete mouse trail

--- js/scripts.js
+++ js/scripts.js
@@ -9,63 +9,6 @@
 
 
 window.addEventListener('DOMContentLoaded', event => {
-
-  var dots = [],
-    mouse = {
-      x: 0,
-      y: 0
-    };
-
-var Dot = function() {
-  this.x = 0;
-  this.y = 0;
-  this.node = (function(){
-    var n = document.createElement("div");
-    n.className = "MouseTrail";
-    document.body.appendChild(n);
-    return n;
-  }());
-};
-
-Dot.prototype.draw = function() {
-  this.node.style.left = this.x + "px";
-  this.node.style.top = this.y + "px";
-};
-
-for (var i = 0; i < 12; i++) {
-  var d = new Dot();
-  dots.push(d);
-}
-
-
-function draw() {
-
-  var x = mouse.x,
-      y = mouse.y;
-      
-  dots.forEach(function(dot, index, dots) {
-    var nextDot = dots[index + 1] || dots[0];
-    
-    dot.x = x;
-    dot.y = y;
-    dot.draw();
-    x += (nextDot.x - dot.x) * .6;
-    y += (nextDot.y - dot.y) * .6;
-
-  });
-}
-
-addEventListener("mousemove", function(event) {
-  mouse.x = event.pageX;
-  mouse.y = event.pageY;
-});
-
-function animate() {
-  draw();
-  requestAnimationFrame(animate);
-}
-
-animate();
   
 	// Navbar shrink function
 	var navbarShrink = function () {
